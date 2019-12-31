f4999d128035e3ecc5d507eb3d15a904dd25c89d
Tyrel Chambers <tychambers3@gmail.com>
2019-12-31 15:41:56 -0500

    ignore exp for now

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,10 +4,10 @@ import config from '../config';
 const authHandler = async (req, res, next) => {
 
   try {
-    if ( !req.headers.token ) return res.status(500).send("No token provided");
+    if ( !req.headers.token ) throw new Error("No token provided");
 
     const token = req.headers.token;
-    const userId = await jwt.verify(token, config[config.env].secret, (err, decoded) => {
+    const userId = await jwt.verify(token, config[config.env].secret, {ignoreExpiration: true}, (err, decoded) => {
       if ( err ) throw new Error(err);
       return decoded.id     
     });
