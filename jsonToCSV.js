2230f18247d26b002e80d5bb8e046c5030a4ac89
Tyrel Chambers <tychambers3@gmail.com>
2020-01-20 19:50:18 -0500

    add sample code

--- jsonToCSV.js
+++ jsonToCSV.js
@@ -1,6 +1,7 @@
 import Axios from 'axios'
 import path from 'path'
-import fs, { write } from 'fs'
+import fs from 'fs'
+import zlib from 'zlib'
 
 let data = "";
 
@@ -20,9 +21,9 @@ let links = [
   "https://www.reddit.com/r/nosleep/comments/c9yeb4/i_am_being_stalked_by_someone_on_the_dark_web/",
   "https://www.reddit.com/r/nosleep/comments/90ksmo/i_bought_a_mystery_box_off_the_dark_web/"
 ]
+const directory = "samples/dark_web";
 
 const main = async () => {
-  const directory = "samples/dark_web";
   const formattedData = [];
 
   makeDir(directory)
@@ -37,6 +38,8 @@ const main = async () => {
   for (let i = 0; i < formattedData.length; i++ ) {
     writeToFile(formattedData[i], i)
   }
+
+
 }
 
 const getData = async () => {
@@ -45,20 +48,19 @@ const getData = async () => {
 
   for ( let i = 0; after !== null; i++ ) {
 
-    await Axios.get(`https://www.reddit.com/r/nosleep/search/.json?q=dark%20web&restrict_sr=1&after=${after}`).then(res => {
+    await Axios.get(`https://www.reddit.com/r/nosleep/search/.json?q=dark%20web&restrict_sr=1&after=${after}&count=50`).then(res => {
       after = res.data.data.after;
       posts = [...posts, ...res.data.data.children]
+
     }).catch(err => err);
   }
 
+  
   return posts;
 }
 
 const writeToFile = (data, i) => {
-  fs.writeFile(`${__dirname}/samples/dark_web/sample_${i}.txt`, data, err => {
-    if (err) throw err;
-    console.log(`Saved file: sample_${i}.txt`)
-  })  
+  fs.writeFileSync(`${__dirname}/${directory}/sample_${i}.txt`, data)  
 }
 const makeDir = async (dir) => {
   fs.mkdir(dir, {recursive: true}, err => {
