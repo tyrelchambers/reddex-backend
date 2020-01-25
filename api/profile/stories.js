b9d66aa1cb0642dde9dd613a1b83a799bcf3b08f
Tyrel Chambers <tychambers3@gmail.com>
2020-01-25 18:36:36 -0500

    fix remove operation for stories

--- api/profile/stories.js
+++ api/profile/stories.js
@@ -152,7 +152,7 @@ app.delete('/stories/remove', authHandler, async (req, res, next) => {
     const {
       postId
     } = req.query;
-    await Story.findOneAndRemove({postId});
+    await Story.deleteOne({postId});
     res.send("Story removed");
   }
 
