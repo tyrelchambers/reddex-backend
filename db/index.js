1d16df7a7403ec1e0026c8dc4d8b0bd17fa0caf1
Tyrel Chambers <tychambers3@gmail.com>
2020-01-30 17:44:15 -0500

    update config

--- db/index.js
+++ db/index.js
@@ -1,3 +1,5 @@
+const pg = require('pg')
+pg.defaults.ssl = true
 const environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile.js')[environment]
 const knex = require('knex')(config);
--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,10 @@ module.exports = {
   },
   staging: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: 'postgres://njayiwdbnwsdib:bf3deaff77dde54b4746aa3cc6e44cb3c19d4eed5bd49d58aed365c74823027d@ec2-52-203-98-126.compute-1.amazonaws.com:5432/dbs3ta5tho30ji?ssl=true',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    }
   }
 }
   
