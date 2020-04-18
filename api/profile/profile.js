8e9a35f29a76411c4f3e3ce3b5e28d50fac12092
Tyrel Chambers <tychambers3@gmail.com>
2020-04-18 16:21:14 -0400

    profile

--- api/profile/profile.js
+++ api/profile/profile.js
@@ -3,6 +3,7 @@ import { authHandler } from '../../middleware/middleware';
 import bcrypt from 'bcryptjs';
 import knex from '../../db/index'
 import User from '../../db/Models/User'
+import StoriesUsed from '../../db/Models/StoriesUsed'
 
 const app = express.Router();
 
@@ -28,7 +29,7 @@ app.post('/youtube', authHandler, async (req, res, next) => {
   try {
     const youtube_id = req.sanitize(req.body.youtube_id);
 
-    User.update({
+    await User.update({
       youtube_id
     },{
       where: {
@@ -51,11 +52,15 @@ app.put('/update/email', authHandler, async (req, res, next) => {
     const email = req.sanitize(req.body.email);
 
     if ( !email ) throw new Error("No email provided");
-    const user = await knex('users').where({
-      uuid: res.locals.userId
-    }).update({
+
+    const user = User.update({
       email
-    }).returning('*')
+    }, {
+      where: {
+        uuid: res.locals.userId
+      }
+    })
+    
     res.send(user)
   }
 
@@ -71,22 +76,27 @@ app.put('/update/password', authHandler, async (req, res, next) => {
 
     if (!newPassword || !currentPassword) throw new Error("No passwords provided");
 
-    const user = await knex('users').where({
-      uuid: res.locals.userId
-    }).returning('*')
-    const comparePasswords = await bcrypt.compareSync(currentPassword, user[0].password);
+    const user = await User.findOne({
+      where: {
+        uuid: res.locals.userId
+      }
+    })
+
+    const comparePasswords = await bcrypt.compareSync(currentPassword, user.password);
 
     if ( !comparePasswords ) throw new Error("Passwords don't match");
 
     const hashNewPassword = await bcrypt.hashSync(newPassword, 10);
 
-    const newUser = await knex('users').where({
-      uuid: res.locals.userId
-    }).update({
+    await User.update({
       password: hashNewPassword
-    }).returning('*')
+    }, {
+      where: {
+        uuid: res.locals.userId
+      }
+    })
     
-    res.send(newUser)
+    res.sendStatus(200)
 
   }
 
@@ -101,9 +111,11 @@ app.delete('/delete', authHandler, async (req, res, next) => {
 
     if ( uuid !== res.locals.userId) throw new Error("Something went wrong");
 
-    await knex('users').where({
-      uuid
-    }).del()
+    await User.destroy({
+      where: {
+        uuid
+      }
+    })
     
     res.send(200)
   }
@@ -116,7 +128,14 @@ app.delete('/delete', authHandler, async (req, res, next) => {
 app.get('/stories_used', authHandler, async (req, res, next) => {
   try {
     const id = res.locals.userId;
-    const stories = await knex('stories_used').where({user_id: id}).returning('*')
+    const stories = await StoriesUsed.findAll({
+      where: {
+        user_id: id
+      }
+    })
+    
+    stories.map(x => x.dataValues)
+
     res.send(stories)
   }
 
@@ -128,13 +147,15 @@ app.get('/stories_used', authHandler, async (req, res, next) => {
 app.post('/reddit_profile', authHandler, async (req, res, next) => {
   try {
     const reddit_profile = req.body;
-    const user = await knex('users').where({
-      uuid: res.locals.userId
-    }).update({
-      reddit_profile: reddit_profile
-    }).returning('*')
+    const user = await User.update({
+      reddit_profile
+    },{
+      where: {
+        uuid: res.locals.userId
+      }
+    })
     
-    res.send(user[0])
+    res.send(user)
   }
 
   catch(err) {
@@ -148,11 +169,14 @@ app.get('/patreon_tier', async (req, res) => {
       user_id
     } = req.query;
 
-    const tier = await knex('users').where({
-      uuid: user_id
-    }).select('patreon_tier')
+    const tier = await User.findOne({
+      where: {
+        uuid: user_id
+      },
+      attributes: ["patreon_tier"]
+    })
 
-    res.send(tier[0])
+    res.send(tier)
   } catch (error) {
     
   }
