3c2aee9e17f26f8aa84b5afc11171362f2d59ccd
Tyrel Chambers <tychambers3@gmail.com>
2020-05-03 17:34:58 -0400

    UPDATE MONGO CONFIG

--- config.js
+++ config.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
+
+
 module.exports = {
   "development": {
     "secret": "my furry cat house",
     "database": "mongodb://localhost/reddex",
-
   },
   "production": {
     "secret": "my furry cat house"
   },
   "staging": {
-    "secret": "my furry cat house"
+    "secret": "my furry cat house",
+    'database': `mongodb://${process.env.STAGING_MONGO_USER}:${process.env.STAGING_MONGO_PASSWORD}@ds017636.mlab.com:17636/heroku_49hrvs85`
   },
   "env": process.env.NODE_ENV || "development"
 }
